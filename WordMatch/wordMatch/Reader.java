c5986c9855742bf521fbd1451e80fc2c6a8aa01e
EthanWooMonkey <92928095+EthanWooMonkey@users.noreply.github.com>
2021-11-02 15:14:56 -0400

    rest of reader

--- WordMatch/wordMatch/Reader.java
+++ WordMatch/wordMatch/Reader.java
@@ -54,4 +54,154 @@ public class Reader
         }
         return arr; 
     }
+    public ArrayList<String> readFour(int x, char a, char b, char c, char d) throws Exception
+    {
+        String inputLine; 
+        ArrayList<String> arr = new ArrayList<String>(); 
+        while((inputLine = in.readLine()) != null)
+        {
+            if(inputLine.charAt(0) == a)
+            {
+                if(inputLine.charAt(1) == b || inputLine.charAt(1) == c || inputLine.charAt(1) == d)
+                {
+                    if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d)
+                    {
+                        if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d)
+                        {
+                            arr.add(inputLine);
+                        }
+                    }
+                }
+            }
+            if(inputLine.charAt(0) == b)
+            {
+                if(inputLine.charAt(1) == a || inputLine.charAt(1) == c || inputLine.charAt(1) == d)
+                {
+                    if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d)
+                    {
+                        if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d)
+                        {
+                            arr.add(inputLine);
+                        }
+                    }
+                }
+            }
+            if(inputLine.charAt(0) == c)
+            {
+                if(inputLine.charAt(1) == b || inputLine.charAt(1) == a || inputLine.charAt(1) == d)
+                {
+                    if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d)
+                    {
+                        if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d)
+                        {
+                            arr.add(inputLine);
+                        }
+                    }
+                }
+            }
+            if(inputLine.charAt(0) == d)
+            {
+                if(inputLine.charAt(1) == b || inputLine.charAt(1) == c || inputLine.charAt(1) == a)
+                {
+                    if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d)
+                    {
+                        if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d)
+                        {
+                            arr.add(inputLine);
+                        }
+                    }
+                }
+            }
+        }
+        return arr; 
+    }
+    public ArrayList<String> readFive(int x, char a, char b, char c, char d, char e) throws Exception
+    {
+        String inputLine; 
+        ArrayList<String> arr = new ArrayList<String>(); 
+        while((inputLine = in.readLine()) != null)
+        {
+            if(inputLine.charAt(0) == a)
+            {
+                if(inputLine.charAt(1) == b || inputLine.charAt(1) == c || inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                {
+                    if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                    {
+                        if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                        {
+                            if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                            {
+                                arr.add(inputLine);
+                            }
+                        }
+                    }
+                }
+            }
+            if(inputLine.charAt(0) == b)
+            {
+                if(inputLine.charAt(1) == a || inputLine.charAt(1) == c || inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                {
+                    if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                    {
+                        if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                        {
+                            if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                            {
+                                arr.add(inputLine);
+                            }
+                        }
+                    }
+                }
+            }
+            if(inputLine.charAt(0) == c)
+            {
+                if(inputLine.charAt(1) == b || inputLine.charAt(1) == a || inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                {
+                    if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                    {
+                        if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                        {
+                            if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                            {
+                                arr.add(inputLine);
+                            }
+                        }
+                    }
+                }
+            }
+            if(inputLine.charAt(0) == d)
+            {
+                if(inputLine.charAt(1) == b || inputLine.charAt(1) == c || inputLine.charAt(1) == a || inputLine.charAt(1) == e)
+                {
+                    if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                    {
+                        if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                        {
+                            if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                            {
+                                arr.add(inputLine);
+                            }
+                        }
+                    }
+                }
+            }
+            if(inputLine.charAt(0) == e)
+            {
+                if(inputLine.charAt(1) == b || inputLine.charAt(1) == c || inputLine.charAt(1) == d || inputLine.charAt(1) == a)
+                {
+                    if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                    {
+                        if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                        {
+                            if(inputLine.charAt(2) == a || inputLine.charAt(0) == b || inputLine.charAt(0) == c|| inputLine.charAt(1) == d || inputLine.charAt(1) == e)
+                            {
+                                arr.add(inputLine);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return arr; 
+    }
 }
